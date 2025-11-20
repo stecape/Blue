454ee4b4e79a1582ae41ec8df87f4303aa287a7a
stecape <ste.cape@gmail.com>
2025-11-20 09:27:39 +0100

    Fixed mqtt address

--- server/src/api/mqtt_api.js
+++ server/src/api/mqtt_api.js
@@ -9,7 +9,7 @@ export default function (app, pool) {
 
   let devices = []
 
-  mqttClient = mqtt.connect("mqtt://mosquitto:1883", {
+  mqttClient = mqtt.connect("mqtt://www.stecape.space:1883", {
     clientId: mqtt_client_id, // Opzionale: identificativo del client
     clean: true, // Opzionale: indica se il broker deve mantenere lo stato del client
   })
